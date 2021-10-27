eb5509370c6bc07fae899d092fd7552ef954ec72
Ashwani Bakshi <ab324002@gmail.com>
2021-10-27 17:48:48 +0530

    commit

--- app/src/app/data.service.ts
+++ app/src/app/data.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private httpService:HttpClient) {
+     
+   }
+   demo(){
+     return "working";
+   }
 }
