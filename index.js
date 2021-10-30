a3afb186c9a0156bb8058daf84cd49cc4b048bda
Ashwani Bakshi <ab324002@gmail.com>
2021-10-30 05:31:28 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express   = require("express");
 const mongoose  = require("mongoose");
+const cors      = require("cors");
 
 
 const app = express();
@@ -10,6 +11,8 @@ mongoose.connect("mongodb://localhost:27017/compdemo")
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+app.use(cors());
 
 app.use('/api/v1',require("./routes/api_routes"));
 
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -5,5 +5,11 @@ const apiController = require("../controller/api_controller");
 
 router.post("/register",apiController.register);
 router.post("/login",apiController.login);
+router.get("/demo",(req,res)=>{
+    console.log("working")
+    let da = [{"name":'working'}];
+        res.json({data:da,msg:"success"});
+});
+
 
 module.exports = router;
